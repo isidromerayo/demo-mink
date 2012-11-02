49251a446447bd49bd8411226c124ed4bfac3666
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-11-02 20:24:09 +0100

    create FeatureContext extends Mink

---
+++ README.md
@@ -0,0 +1,23 @@
+Demo Mink
+=========
+
+Install
+-------
+
+$ composer.phar install
+
+Use
+---
+
+Single feature
+
+$ bin/behat features/search.feature
+
+All features
+
+$ bin/behat
+
+Using Selenium RC, you need to launch service.
+
+$ sh ./launch-selenium.sh
+
---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,23 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+use Behat\MinkExtension\Context\MinkContext;
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+}
